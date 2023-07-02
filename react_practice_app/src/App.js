38e9d7ccc3b4b0ae322541c315f11cb084dd0ad7
Krishna Kumar Siripuram <krishnakumar.siripuram@techwave.net>
2023-07-02 19:00:51 +0530

    ref,portal etc

--- react_practice_app/src/App.js
+++ react_practice_app/src/App.js
@@ -1,18 +1,71 @@
+import React, { useState, useRef } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
-import NewExpense  from "./components/NewExpense/NewExpense";
+import NewExpense from "./components/NewExpense/NewExpense";
+import './components/NewExpense/ExpenseForm.css';
 
 function MyApp() {
+  let EXPENSE_ITEMS = [
+    {
+      key: 0,
+      title: "Cloths Purchase",
+      amount: 490.0,
+      date: new Date(2023, 2, 30),
+    },
+    {
+      key: 1,
+      title: "Shoes Purchase",
+      amount: 590.0,
+      date: new Date(2022, 2, 30),
+    },
+    {
+      key: 2,
+      title: "Perfume Purchase",
+      amount: 690.0,
+      date: new Date(2021, 2, 30),
+    },
+    {
+      key: 3,
+      title: "Watch Purchase",
+      amount: 390.0,
+      date: new Date(2020, 2, 30),
+    },
+  ];
+
+  const userNameRef = useRef();
+
+  const accessRefValue = (event) => {
+    event.preventDefault();
+    alert(userNameRef.current.value);
+
+    userNameRef.current.value = 'updated value by ref.';
+  };
+  const [expenseValues, setExpenseValues] = useState(EXPENSE_ITEMS);
 
   const AddNewExpenseDataHandler = (expenseValue) => {
-    alert(" Id:  "+ expenseValue.id +"\n Title: "+expenseValue.title+"\n Amount: "+ expenseValue.amount+"\n Date: "+ expenseValue.date);
-  }
+    setExpenseValues((prevExpenses) => {
+      return [expenseValue, ...prevExpenses];
+    });
+  };
   return (
     <div>
-        <NewExpense onAddExpenseData={AddNewExpenseDataHandler}></NewExpense>
+      <NewExpense onAddExpenseData={AddNewExpenseDataHandler}></NewExpense>
       <div>
-        <Expenses />
+        <Expenses expenses={expenseValues} />
       </div>
+
+      <br />
+      {/* ref example */}
+      <form onSubmit={accessRefValue}>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <input type="text" ref={userNameRef}></input>
+            </div>
+            <div className="new-expense__actions">
+              <button type="submit"> Check Ref Input Value.</button>
+            </div>
+        </div>
+      </form>
     </div>
   );
 }
--- react_practice_app/src/components/Expenses/ExpenseDate.js
+++ react_practice_app/src/components/Expenses/ExpenseDate.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 export default function ExpenseDate(props)
 {
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+    const month = new Date(props.date).toLocaleString("en-US", { month: "long" });
+   const year = new Date(props.date).getFullYear();
+  const day = new Date(props.date).toLocaleString("en-US", { day: "2-digit" });
     return(
     <div className="expense-date">
-        <div className="expense-date__month"> {month}</div>
+        <div className="expense-date__month" > {month}</div>
         <div className="expense-date__year">{year}</div>
         <div className="expense-date__day">{day}</div>
       </div>
